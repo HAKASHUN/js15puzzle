a8832746a2ed26c6d161002b175e440d8664559e
yshrsmz <the.phantom.bane@gmail.com>
2012-11-13 21:58:23 +0900

    optimize the way to manage tile alignment

--- js/puzzle.js
+++ js/puzzle.js
@@ -29,22 +29,30 @@ Puzzle.prototype._position = [];
 
 Puzzle.prototype._board = [];
 
-Puzzle.prototype._createTile = function(position){
+Puzzle.prototype._createTile = function(count){
 	var tile = document.createElement('div');
-	if (position === this.config.totalTileCount) {
-		tile.classList.add('emptyTile');
+	tile.classList.add('tile');
+	tile.setAttribute('data-tile-count', count);
+	if (count === this.config.totalTileCount) {
+		tile.classList.add('empty');
 	} else {
-		tile.classList.add('tile');
-		tile.appendChild(document.createTextNode(position));
+		tile.appendChild(document.createTextNode(count));
 		tile.addEventListener('click', this._onTileClick);
 	}
 	return tile;
 };
 
-Puzzle.prototype._getPosition = function(y, x){
+/**
+ * タイルのx, yから実際の座標(px)を取得
+ * @param x
+ * @param y
+ * @return {Object}
+ * @private
+ */
+Puzzle.prototype._getAbsolutePosition = function(x, y){
 	return {
-		y: y,
 		x: x,
+		y: y,
 		left: (x * this.config.tileSize + this.config.margin * (x + 1)),
 		top: (y * this.config.tileSize + this.config.margin * (y + 1))
 
@@ -55,48 +63,58 @@ Puzzle.prototype._onTileClick = function(e){
 	e.preventDefault();
 };
 
+/**
+ * タイルを初期化
+ */
 Puzzle.prototype.init = function(){
 	this.config.totalTileCount = Math.pow(this.config.tileCount, 2);
 	this._boardElement = document.querySelector(this.config.boardSelector);
 	this._boardElement.innerHTML = '';
 	this._position = [];
 	this._board = [];
+
 	//盤面の作成
-	for (var y = 0; y < this.config.tileCount; y++) {
-		this._board[y] = [];
-		for (var x = 0; x < this.config.tileCount; x++) {
-			var tile = this._createTile((y * this.config.tileCount + x + 1));
-			var positionObj = this._getPosition(y, x);
-			tile.style.left = positionObj.left + 'px';
-			tile.style.top = positionObj.top + 'px';
-			this._position.push(positionObj);
-			this._board[y][x] = tile;
-			this._boardElement.appendChild(tile);
-		}
+	for (var i = 0; i < this.config.totalTileCount; i++) {
+		var tile = this._createTile(i + 1),
+			curPos = this._getPositionFromCount(i),
+			positionObj = this._getAbsolutePosition(curPos.x, curPos.y);
+
+		tile.style.left = positionObj.left + 'px';
+		tile.style.top = positionObj.top + 'px';
+		tile.setAttribute('data-current-count', (i + 1));
+		this._position.push(positionObj);
+		this._board[i] = tile;
+		this._boardElement.appendChild(tile);
 	}
 	this._answer = this._board.concat();
 };
 
+/**
+ * タイルをランダムに並び替え
+ * @return {*}
+ */
 Puzzle.prototype.randomize = function(){
-	var ary = this._getShuffledArray(this._getSerialArray(this.config.totalTileCount));
-	var tempAry = [];
+	var ary = this._getShuffledArray(this._getSerialArray(this.config.totalTileCount)),
+		tempAry = [];
+
 	this._boardElement.innerHTML = '';
-	var tileCount = 0;
 	console.log('ary:', ary);
-	for (var x = 0, lenX = this.config.tileCount; x < lenX; x++) {
-		tempAry[x] = [];
-		for (var y = 0, lenY = this.config.tileCount; y < lenY; y++) {
-			var curPos = this._getPositionFromCount(ary[tileCount]);
-			tempAry[x][y] = this._board[curPos.x][curPos.y];
-			tempAry[x][y].style.left = this._position[tileCount].left + 'px';
-			tempAry[x][y].style.top = this._position[tileCount].top + 'px';
-			this._boardElement.appendChild(tempAry[x][y]);
-			tileCount++;
-		}
+	for (var i = 0; i < this.config.totalTileCount; i++) {
+		tempAry[i] = this._board[ary[i]];
+		tempAry[i].style.left = this._position[i].left + 'px';
+		tempAry[i].style.top = this._position[i].top + 'px';
+		tempAry[i].setAttribute('data-current-count', (i + 1));
+		this._boardElement.appendChild(tempAry[i]);
 	}
 	return ary;
 };
 
+/**
+ *
+ * @param count
+ * @return {Array}
+ * @private
+ */
 Puzzle.prototype._getSerialArray = function(count){
 	var resultAry = [];
 	for (var i = 0; i < count; i++) {
@@ -105,16 +123,29 @@ Puzzle.prototype._getSerialArray = function(count){
 	return resultAry;
 };
 
+/**
+ *
+ * @param ary
+ * @return {Array}
+ * @private
+ */
 Puzzle.prototype._getShuffledArray = function(ary){
 	var len = ary.length,
 		tempAry = ary.concat(),
 		result = [];
+
 	while(len) {
 		result.push(tempAry.splice(Math.floor(Math.random() * len--), 1)[0]);
 	}
 	return result;
 };
 
+/**
+ *
+ * @param count
+ * @return {Object}
+ * @private
+ */
 Puzzle.prototype._getPositionFromCount = function(count){
 	var result = {};
 	var tempCount = count + 1;
@@ -123,6 +154,12 @@ Puzzle.prototype._getPositionFromCount = function(count){
 	return result;
 };
 
+/**
+ *
+ * @param position
+ * @return {*}
+ * @private
+ */
 Puzzle.prototype._getCountFromPosition = function(position){
 	//position = { x: int, y: int }
 	return this.config.tileCount * position.y + (position.x + 1);
